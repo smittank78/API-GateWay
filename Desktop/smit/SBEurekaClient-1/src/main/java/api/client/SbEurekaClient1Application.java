3ad3ea664ba1ce560e35514db4957d28ab052f08
smittank78 <sjtank.781@gmail.com>
2022-11-30 12:47:36 +0530

    client-1

---
+++ Desktop/smit/SBEurekaClient-1/src/main/java/api/client/SbEurekaClient1Application.java
@@ -0,0 +1,21 @@
+package api.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class SbEurekaClient1Application 
+{
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbEurekaClient1Application.class, args);
+	}
+
+}
